bc1fe780cb2b2e62d765d4c379398994ae52a8dd
Ashraf-2 <hasanashraful03@gmail.com>
2023-10-24 22:17:26 +0600

    added banner

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../../Components/Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            this is home
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root.jsx';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/about",
+        element: <About></About>
+      }
+
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='max-w-6xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
+)
